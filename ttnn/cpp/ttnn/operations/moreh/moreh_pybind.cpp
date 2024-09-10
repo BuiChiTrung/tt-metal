4c3e783e5d0aefcf6cda38996d2c2f3a4bf34f45
BuiChiTrung <trungcspntl@gmail.com>
2024-09-10 10:55:19 +0000

    #12439: add draft code

--- ttnn/cpp/ttnn/operations/moreh/moreh_pybind.cpp
+++ ttnn/cpp/ttnn/operations/moreh/moreh_pybind.cpp
@@ -6,10 +6,12 @@
 
 #include "ttnn/operations/moreh/moreh_adam/moreh_adam_pybind.hpp"
 #include "ttnn/operations/moreh/moreh_arange/moreh_arange_pybind.hpp"
+#include "ttnn/operations/moreh/moreh_nll_loss_unreduced_backward/moreh_nll_loss_unreduced_backward_pybind.hpp"
 
 namespace ttnn::operations::moreh {
 void bind_moreh_operations(py::module &module) {
     moreh_arange::bind_moreh_arange_operation(module);
     moreh_adam::bind_moreh_adam_operation(module);
+    moreh_nll_loss_unreduced_backward::bind_moreh_nll_loss_unreduced_backward_operation(module);
 }
 }  // namespace ttnn::operations::moreh
