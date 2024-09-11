fc371197dac226c9e142b5234d14b25ab511573d
BuiChiTrung <trungcspntl@gmail.com>
2024-09-11 04:46:03 +0000

    #12439: add nll_loss_bwd_reduced draft code

--- ttnn/cpp/ttnn/operations/moreh/moreh_pybind.cpp
+++ ttnn/cpp/ttnn/operations/moreh/moreh_pybind.cpp
@@ -6,6 +6,7 @@
 
 #include "ttnn/operations/moreh/moreh_adam/moreh_adam_pybind.hpp"
 #include "ttnn/operations/moreh/moreh_arange/moreh_arange_pybind.hpp"
+#include "ttnn/operations/moreh/moreh_nll_loss_backward/moreh_nll_loss_backward_pybind.hpp"
 #include "ttnn/operations/moreh/moreh_nll_loss_unreduced_backward/moreh_nll_loss_unreduced_backward_pybind.hpp"
 
 namespace ttnn::operations::moreh {
@@ -13,5 +14,6 @@ void bind_moreh_operations(py::module &module) {
     moreh_arange::bind_moreh_arange_operation(module);
     moreh_adam::bind_moreh_adam_operation(module);
     moreh_nll_loss_unreduced_backward::bind_moreh_nll_loss_unreduced_backward_operation(module);
+    moreh_nll_loss_backward::bind_moreh_nll_loss_backward_operation(module);
 }
 }  // namespace ttnn::operations::moreh
